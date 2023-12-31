2b49050e5a5993b3023298d3017318cbce21bccb
VelDLumina2942 <145996670+VelDLumina2942@users.noreply.github.com>
2024-01-01 04:43:20 +0900

    テスト通せませんでした

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -2,11 +2,39 @@
 #include "Windows.h"                    // Windows API の機能定義
 
 #include "../include/lib_func.h"
-
+void swap(item* p, item* q)
+{
+	item temp;
+	temp = *p;
+	*p = *q;
+	*q = temp;
+}
 // クイックソート(引数が不適切であればfalseを返す)
 bool quick_sort(item* begin, const item* end)
 {
 	// ToDo: クイックソートで並び替えてください
-
-	return false;
+	if (begin == NULL || end < begin)
+		return false;
+	item* pivot;
+	pivot = begin;
+	item* p = begin;
+	item* q = begin + (end - begin - 1);
+	while (p->key<q->key) {
+		while (p->key < pivot->key)
+		{
+			p->key++;
+		}
+		while (q->key > pivot->key)
+		{
+			q->key--;
+		}
+			item temp = *p;
+			*p = *q;
+			*q = temp;
+			
+	}
+	
+	quick_sort(p, pivot-1);
+	quick_sort(pivot+1,q);
+	return true;
 }
