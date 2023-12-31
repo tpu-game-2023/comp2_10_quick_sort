536d97fb4f0afdf9a6ed5c1cf416106636da57ee
deigo6125 <g2327022@outlook>
2023-12-31 11:58:16 +0900

    テストを通しました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -3,10 +3,51 @@
 
 #include "../include/lib_func.h"
 
+void swap(item* begin, const item* end, int a, int b)
+{
+	item tmp;
+	tmp = *(begin + a);
+	*(begin + a) = *(begin + b);
+	*(begin + b) = tmp;
+}
+
+int partition(item* begin, const item* end, int m, int n)
+{
+	for (int i = m; i < n; i++)
+	{
+		if ((begin + i)->key <= (begin + n)->key)
+		{
+			swap(begin, end, m, i);
+			m++;
+		}
+	}
+
+	swap(begin, end, n, m);
+
+	return m;
+}
+
+void sort(item* begin, const item* end, int m, int n)
+{
+	if (m < n)
+	{
+		int x = partition(begin, end, m, n);
+		sort(begin, end, m, x - 1);
+		sort(begin, end, x + 1, n);
+	}
+}
+
 // クイックソート(引数が不適切であればfalseを返す)
 bool quick_sort(item* begin, const item* end)
 {
 	// ToDo: クイックソートで並び替えてください
+	if (begin == NULL || end == NULL || end < begin)
+	{
+		return false;
+	}
 
-	return false;
+	int n = end - begin;
+	sort(begin, end, 0, n - 1);
+
+	return true;
 }
