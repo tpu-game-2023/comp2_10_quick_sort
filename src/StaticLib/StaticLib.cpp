95e6e85716b5cd88fecef650b269dd9624532480
yochi513 <yojrdkt@gmail.com>
2024-01-13 18:29:30 +0900

    テスト確認

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -3,10 +3,57 @@
 
 #include "../include/lib_func.h"
 
+
+typedef struct item_
+{
+    int key;
+    char value[256]; 
+}item;
+
+
 // クイックソート(引数が不適切であればfalseを返す)
 bool quick_sort(item* begin, const item* end)
 {
-	// ToDo: クイックソートで並び替えてください
+	if (begin == NULL || end == NULL || begin >= end)
+	{
+		return false;
+	}
+	item* pivot = begin + (end - begin) / 2;
+	item* i = begin;
+	item* j = end - 1;
+    while (i <= j)
+    {
+        // ピボットより小さい要素を左に、大きい要素を右に移動
+        while (i->key < pivot->key)
+        {
+            ++i;
+        }
+        while (j->key > pivot->key)
+        {
+            --j;
+        }
 
-	return false;
+        if (i <= j)
+        {
+           
+            item temp = *i;
+            *i = *j;
+            *j = temp;
+            ++i;
+            --j;
+        }
+    }
+
+    // 再帰的に左右の部分をソート
+    if (begin < j + 1)
+    {
+        quick_sort(begin, j + 1);
+    }
+    if (i < end)
+    {
+        quick_sort(i, end);
+    }
+
+    return true;
+
 }
