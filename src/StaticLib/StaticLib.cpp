c872c4bcb4095e1d47f59db9751a5bfce89840d3
mana0629 <ysangyuan885@gmail.com>
2024-01-15 01:12:00 +0900

    テストを通しました。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,7 +6,41 @@
 // クイックソート(引数が不適切であればfalseを返す)
 bool quick_sort(item* begin, const item* end)
 {
-	// ToDo: クイックソートで並び替えてください
+    if (begin == NULL || end == NULL || end <= begin) return false;
 
-	return false;
+    QSort(begin, 0, end - begin - 1);
+    return true;
+}
+
+void QSort(item* x, int left, int right)
+{
+    int i, j;
+    int pivot;
+    item tmp;
+    int size = sizeof(tmp);
+
+    i = left;                     
+    j = right;                    
+
+    pivot = x[(left + right) / 2].key; 
+
+    while (1) {
+        while (x[i].key < pivot)      
+            i++;                   
+        while (pivot < x[j].key)     
+            j--;                  
+        if (i >= j)               
+            break;                
+
+        memcpy(&tmp, x + i, size);   
+        memcpy(x + i, x + j, size);
+        memcpy(x + j, &tmp, size);     
+        i++;                      
+        j--;
+    }
+
+    if (left < i - 1)             
+        QSort(x, left, i - 1);     
+    if (j + 1 < right)           
+        QSort(x, j + 1, right);   
 }
--- src/include/lib_func.h
+++ src/include/lib_func.h
@@ -15,7 +15,7 @@ extern "C" {
 
 	// クイックソート(引数が不適切であればfalseを返す)
 	bool quick_sort(item *begin, const item* end);
-
+	void QSort(item* x, int left, int right);
 	// C++ でCのライブラリを使うときのおまじない
 #ifdef __cplusplus
 } // extern "C"
