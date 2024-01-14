8ae578c980f45cd3d8bd32dfdf1ad18cfff9db2d
origod <onigot0609@gmail.com>
2024-01-15 02:44:13 +0900

    テストを通しました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -3,29 +3,46 @@
 
 #include "../include/lib_func.h"
 
+static void swap(item* i1, item* i2)
+{
+	item tmp = *i1;
+	*i1 = *i2;
+	*i2 = tmp;
+}
+
 // クイックソート(引数が不適切であればfalseを返す)
 bool quick_sort(item* begin, const item* end)
 {
 	
-		item* p = begin;
-		item* q = end;
-		item* pivot = (p + q) / 2;
-		while (1)
-		{
-			while (p < pivot)
-				p++;
-			while (pivot < q)
-				q--;
-			if (p >= q)
-				break;
-			item tmp = *p;
-			*p = *q;
-			*q = tmp;
-			p++;
-			q--;
-		}
-	}
+	if (begin == NULL || end == NULL)return false;
+	if (end - begin < 1)return false;
+
+	if (end - begin <= 1)return true;
+
+	int pivot = begin->key;
+
+	item* front = begin + 1;
+	item* back = begin + (end - begin) - 1;
+	
 	// ToDo: クイックソートで並び替えてください
+	while (true)
+	{
+		while (front->key <= pivot && front < end)
+			front++;
+		while (pivot <= back->key && begin < back)
+			back--;
+
+		if (back < front)
+			break;
+
+		swap(front, back);
+	};
+
+	if (back != begin)
+		swap(begin, back);
+
+	quick_sort(begin, back);
+	quick_sort(back + 1, end);
 
 	return true;
 }
