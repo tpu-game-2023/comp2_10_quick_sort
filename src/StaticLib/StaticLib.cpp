37d1972e228e121a106855baa893a44874d12144
ayuzakana84 <ayunume@gmail.com>
2024-01-14 20:51:37 +0900

    テストを通しました。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -3,10 +3,51 @@
 
 #include "../include/lib_func.h"
 
+int Partitioning(item* begin, const item* end, int a, int b)
+{
+	item num;
+
+	for (int i = a; i < b; i++)
+	{
+		if ((begin + i)->key <= (begin + b)->key)
+		{
+			num = *(begin + a);
+			*(begin + a) = *(begin + i);
+			*(begin + i) = num;
+
+			a++;
+		}
+	}
+
+	num = *(begin + b);
+	*(begin + b) = *(begin + a);
+	*(begin + a) = num;
+
+	return a;
+}
+
+void main_sort(item* begin, const item* end, int a, int b)
+{
+	if (a < b)
+	{
+		int pivot = Partitioning(begin, end, a, b);
+
+		main_sort(begin, end, a, pivot - 1);
+		main_sort(begin, end, pivot + 1, b);
+	}
+}
+
 // クイックソート(引数が不適切であればfalseを返す)
 bool quick_sort(item* begin, const item* end)
 {
 	// ToDo: クイックソートで並び替えてください
+	if (begin != 0 && end != 0 && begin <= end)
+	{
+		int num = end - begin;
+		
+		main_sort(begin, end, 0, num - 1);
 
+		return true;
+	}
 	return false;
 }
