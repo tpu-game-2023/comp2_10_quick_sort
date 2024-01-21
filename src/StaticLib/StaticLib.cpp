a810e22cd9a426c986ce782a6b058f38a30ceecd
diceroll5656 <kuni0521iku@icloud.com>
2024-01-22 00:04:34 +0900

    通しました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -3,10 +3,45 @@
 
 #include "../include/lib_func.h"
 
+int partition(item array[], int low, int high) {
+    int pivot = array[high].key;
+    int i = low - 1;
+
+    for (int j = low; j < high; ++j) {
+        if (array[j].key < pivot) {
+            ++i;
+            item temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+    item temp = array[i + 1];
+    array[i + 1] = array[high];
+    array[high] = temp;
+
+    return i + 1;
+}
+
+// クイックソートの再帰的な実装
+void quickSort(item array[], int low, int high) {
+    if (low < high) {
+
+        int partitionIndex = partition(array, low, high);
+        quickSort(array, low, partitionIndex - 1);
+        quickSort(array, partitionIndex + 1, high);
+
+    }
+}
+
+
 // クイックソート(引数が不適切であればfalseを返す)
 bool quick_sort(item* begin, const item* end)
 {
 	// ToDo: クイックソートで並び替えてください
+    if (begin == NULL || end == NULL || end < begin)return false;
 
-	return false;
+    quickSort(begin, 0, end - begin - 1);
+
+    return true;
 }
